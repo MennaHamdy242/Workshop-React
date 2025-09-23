ba43ad7f06f75f878a2db765d7615120c746242f
helen-an8 <nesaliyou990@gmail.com>
2025-09-23 19:44:39 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Blog from './pages/Blog';
-import Home from './pages/Home'
-import Shop from './pages/Shop'
-import Contact from './pages/Contact'
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Contact from './pages/Contact';
 import About from "./pages/AboutUs";
-import Checkout from './pages/Checkout'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
+import Checkout from './pages/Checkout';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import "./App.css"; 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<Navigate to="/home" replace />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/meats" element={<Shop />} />
-          <Route path="/bakery" element={<Shop />} />
-          <Route path="/beverages" element={<Shop />} />
-          <Route path="/blog" element={<Blog />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} /> 
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Navigate to="/home" replace />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/shop" element={<Shop />} />
+            <Route path="/meats" element={<Shop />} />
+            <Route path="/bakery" element={<Shop />} />
+            <Route path="/beverages" element={<Shop />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/about" element={<About />} /> 
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
-export default App
+
+export default App;
