2996e57a6421c0f9fbdfb1120e4160ad2bd9eded
Menna Hamdy <minah2425@gmail.com>
2025-09-24 18:11:30 +0300

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Cart({ cart, addToCart, removeFromCart }) {
-  const totalPrice = cart.reduce((sum, item) => sum + item.price * item.quantity, 0).toFixed(2);
+  const navigate = useNavigate();
+  const totalPrice = cart
+    .reduce((sum, item) => sum + item.price * item.quantity, 0)
+    .toFixed(2);
 
   if (cart.length === 0) {
     return (
@@ -14,33 +18,79 @@ function Cart({ cart, addToCart, removeFromCart }) {
 
   return (
     <div style={{ padding: "20px" }}>
-      <h2><b>Shopping Cart</b></h2>
-      <div style={{ display: "flex", flexDirection: "column", gap: "20px",  }}>
+      <h2>
+        <b>Shopping Cart</b>
+      </h2>
+      <div style={{ display: "flex", flexDirection: "column", gap: "20px" }}>
         {cart.map((item) => (
-          <div key={item.id} style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-between",
-            border: "1px solid #ccc",
-            padding: "10px",
-            borderRadius: "8px"
-          }}>
+          <div
+            key={item.id}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+              border: "1px solid #ccc",
+              padding: "10px",
+              borderRadius: "8px"
+            }}
+          >
             <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
               <img
                 src={`http://localhost:5000${item.imageFront}`}
                 alt={item.name}
-                style={{ width: "120px", height: "100px", objectFit: "cover", borderRadius: "12px" }}
+                style={{
+                  width: "120px",
+                  height: "100px",
+                  objectFit: "cover",
+                  borderRadius: "12px"
+                }}
               />
               <div>
-                <h4 style={{ margin: 0, fontWeight:'bold' }}>{item.name}</h4>
-                <p style={{ margin: "5px 0" , color:'#28857aff', fontWeight:'bold'  }}>Price: ${item.price}</p>
-                <p style={{ margin: "5px 0", color:'#28857aff', fontWeight:'bold'  }}>Subtotal: ${(item.price * item.quantity).toFixed(2)}</p>
+                <h4 style={{ margin: 0, fontWeight: "bold" }}>{item.name}</h4>
+                <p
+                  style={{
+                    margin: "5px 0",
+                    color: "#28857aff",
+                    fontWeight: "bold"
+                  }}
+                >
+                  Price: ${item.price}
+                </p>
+                <p
+                  style={{
+                    margin: "5px 0",
+                    color: "#28857aff",
+                    fontWeight: "bold"
+                  }}
+                >
+                  Subtotal: ${(item.price * item.quantity).toFixed(2)}
+                </p>
               </div>
             </div>
-            <div style={{ display: "flex", alignItems: "center", gap: "20px",  }}>
-              <button onClick={() => removeFromCart(item.id)} style={{ padding: "5px 10px", width:'60px', backgroundColor:'#3fb4a6ff' , color:'white' }}>-</button>
+            <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
+              <button
+                onClick={() => removeFromCart(item.id)}
+                style={{
+                  padding: "5px 10px",
+                  width: "60px",
+                  backgroundColor: "#3fb4a6ff",
+                  color: "white"
+                }}
+              >
+                -
+              </button>
               <span>{item.quantity}</span>
-              <button onClick={() => addToCart(item)} style={{ padding: "5px 10px", width:'60px',  backgroundColor:'#3fb4a6ff', color:'white'}}>+</button>
+              <button
+                onClick={() => addToCart(item)}
+                style={{
+                  padding: "5px 10px",
+                  width: "60px",
+                  backgroundColor: "#3fb4a6ff",
+                  color: "white"
+                }}
+              >
+                +
+              </button>
             </div>
           </div>
         ))}
@@ -49,24 +99,25 @@ function Cart({ cart, addToCart, removeFromCart }) {
         <strong>Total: ${totalPrice}</strong>
       </div>
 
-      
-
-
-<div style={{  display:"flex", justifyContent:'center',  marginTop: "20px" }}>
- 
-    <button style={{
-      padding: "19px 20px",
-      backgroundColor: "#28857aff",
-      color: "#fff",
-      border: "none",
-      borderRadius: "6px",
-      cursor: "pointer",
-      fontSize: "16px", 
-      marginLeft: "-20px"
-    }}>
-      Continue to Checkout
-    </button>
-</div>
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
+      >
+        <button
+          onClick={() => navigate("/checkout", { state: { cart } })}
+          style={{
+            padding: "19px 20px",
+            backgroundColor: "#28857aff",
+            color: "#fff",
+            border: "none",
+            borderRadius: "6px",
+            cursor: "pointer",
+            fontSize: "16px",
+            marginLeft: "-20px"
+          }}
+        >
+          Continue to Checkout
+        </button>
+      </div>
     </div>
   );
 }
